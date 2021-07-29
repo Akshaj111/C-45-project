b2997ca45a89d6aee34eb77eab948a976fdb2f86
Akshaj111 <76700912+Akshaj111@users.noreply.github.com>
2021-07-29 13:35:03 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var backG;
+var playImg;
+var gameState = 0;
+var startImg;
+var bg2;
+var ast1, ast2;
+
+function preload(){
+    backG = loadImage("backGMarsImg.jpg");
+    playImg = loadImage("play.jpg");
+    startImg = loadImage("start.jpg");
+    bg2 = loadImage("galaxy.jpg");
+    ast1 = loadImage("astronaut1.jpg");
+    ast2 = loadImage("astronaut2.jpg");
+}
+
+function setup(){
+createCanvas(windowWidth,windowHeight);
+}
+
+function draw (){
+    
+    background(backG);
+    if(gameState===0){
+        image(playImg,displayWidth/2-60,displayHeight/5,100,100);
+        textSize(30);
+        fill("black");
+        text("Press SPACE to begin!",displayWidth/2-160,displayHeight/2);
+    }
+
+    if(keyDown("SPACE")){
+        gameState = 1;
+    }
+
+    if(gameState===1){
+        background(backG);    
+        textSize(25);
+        fill("blue");
+        text("Rules: \n  You need to use the arrow keys to jump up and down the Buttes and craters to get to the lander. \n  However, the dust storms and crumbly rocks will do whatever they can to stop you. \n   If you run into a health power pill, then the dust storms will stop moving \n and you have 30 extra seconds to run. If you crash, 1 of your 5 lives will vanish. \n Use the rover token where possible so you can get there quickly.\n\n Press the 's' key if you dare to take the challenge!",120,displayHeight/4);
+    }
+
+    if(keyDown("s")){
+        gameState=2;
+    }
+
+    if(gameState===2){
+    background(bg2);
+    image(startImg,displayWidth/2-120,displayHeight/4,200,200);
+    }   
+
+    if(keyDown("A")){
+        gameState=3;
+    }
+    if(gameState===3){
+       startImg.isVisible=false;
+       image(ast1,displayWidth/2-50,displayHeight/2+50,130,300);
+       image(ast2,displayWidth/2+80,displayHeight/2+50,130,300);
+    }
+}
